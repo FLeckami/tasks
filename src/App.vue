382f00aa29133b16135df455e4547dd43af0c103
FLeckami <floch210@hotmail.fr>
2021-12-01 10:48:33 +0100

    supression logo Vuetify

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,6 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
       <v-app-bar-title>Gestion de tâche</v-app-bar-title>
 
     </v-app-bar>
